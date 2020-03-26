251603fc020b82f4c3b195058ffcbc05e8816ed4
Ali Serag <mr.ali.serag.eldin@gmail.com>
2020-03-25 17:36:49 -0700

    News updates

--- app/src/pages/News.vue
+++ app/src/pages/News.vue
@@ -11,8 +11,92 @@
         Relevant events for your business
       </div>
     </div>
+
     <div class="news__con">
       <div class="news__con--content">
+        <div class="text-muted text-right">
+          <small> March 25, 2020</small>
+        </div>
+
+        <b>Canada </b>Shopify has compiled resources, information and tools to
+        support Canadian businesses and help them navigate through the COVID-19
+        crisis, including $200 million in small business funding. Click here to
+        learn more.
+        <span
+          ><a
+            href="https://www.shopify.com/covid19?fbclid=IwAR2FdEkXufDTxKXMTUf080jBjV8U1IGWDQDRRPhsoByoDMa8mS2HIl_x7LA"
+            >[Source]</a
+          ></span
+        >
+      </div>
+
+      <div class="news__con--content">
+        <div class="text-muted text-right">
+          <small> March 25, 2020</small>
+        </div>
+
+        <b>OTTAWA—Canada </b>Federal government is looking for organizations
+        that can supply emergency products and innovative solutions to support
+        the fight against COVID-19.
+        <span
+          ><a
+            href="https://buyandsell.gc.ca/calling-all-suppliers-help-canada-combat-covid-19"
+            >[Source]</a
+          ></span
+        >
+      </div>
+
+      <div class="news__con--content">
+        <div class="text-muted text-right">
+          <small> March 25, 2020</small>
+        </div>
+
+        <b>OTTAWA—Canada </b>Federal government is looking for organizations
+        that can supply emergency products and innovative solutions to support
+        the fight against COVID-19.
+        <span
+          ><a
+            href="https://buyandsell.gc.ca/calling-all-suppliers-help-canada-combat-covid-19"
+            >[Source]</a
+          ></span
+        >
+      </div>
+
+      <div class="news__con--content">
+        <div class="text-muted text-right">
+          <small> March 25, 2020</small>
+        </div>
+
+        <b>OTTAWA—Canada </b>60+ Leading Canadian Business Organizations Call on
+        Government to Protect Jobs and Postpone Non-essential New Measures.
+        <span
+          ><a
+            href="https://chamber.us1.list-manage.com/track/click?u=7ff6d535f16100c2e37d2ef8b&id=00bfecda41&e=5ac17939a9"
+            >[Source]</a
+          ></span
+        >
+      </div>
+
+      <div class="news__con--content">
+        <div class="text-muted text-right">
+          <small> March 24, 2020</small>
+        </div>
+
+        <b>TORONTO—Canada </b> Ontario Providing Electricity Relief to support
+        Small Businesses and Farms During COVID-19
+        <span
+          ><a
+            href="https://news.ontario.ca/opo/en/2020/3/ontario-providing-electricity-relief-to-families-small-businesses-and-farms-during-covid-19.html"
+            >[Source]</a
+          ></span
+        >
+      </div>
+
+      <div class="news__con--content">
+        <div class="text-muted text-right">
+          <small> March 18, 2020</small>
+        </div>
+
         <b>OTTAWA—Canada </b>announced a fiscal-stimulus and tax-deferral
         package amounting to nearly 4% of its gross domestic product, marking an
         aggressive effort to contain economic damage caused by the coronavirus
@@ -25,7 +109,10 @@
         >
       </div>
       <div class="news__con--content">
-        <b> VANCOUVER—Canada</b> announced a fiscal-stimulus and tax-deferral
+        <div class="text-muted text-right">
+          <small> March 18, 2020</small>
+        </div>
+        <b>OTTAWA—Canada</b> announced a fiscal-stimulus and tax-deferral
         package amounting to nearly 4% of its gross domestic product.
         <span
           ><a
@@ -35,7 +122,10 @@
         >
       </div>
       <div class="news__con--content">
-        <b>MONTREAL—Canada </b>announced a fiscal-stimulus and tax-deferral
+        <div class="text-muted text-right">
+          <small> March 18, 2020</small>
+        </div>
+        <b>OTTAWA—Canada </b>announced a fiscal-stimulus and tax-deferral
         package .
         <span
           ><a
@@ -45,7 +135,10 @@
         >
       </div>
       <div class="news__con--content">
-        <b>VANCOUVER—Canada</b> announced a fiscal-stimulus and tax-deferral
+        <div class="text-muted text-right">
+          <small> March 18, 2020</small>
+        </div>
+        <b>OTTAWA—Canada</b> announced a fiscal-stimulus and tax-deferral
         package amounting to nearly 4% of its gross domestic product.
         <span
           ><a
@@ -100,6 +193,12 @@ export default {
   font-weight: bold;
 }
 
+.news__con--time-stamp {
+  position: relative;
+  right: 0px;
+  top: 0px;
+}
+
 @media screen and (min-width: 768px) {
   .news__con--content {
     width: 768px;
@@ -109,7 +208,7 @@ export default {
 }
 @media screen and (min-width: 768px) {
   .news__con {
-    height: 100vh;
+    height: 120vh;
   }
 }
 </style>
